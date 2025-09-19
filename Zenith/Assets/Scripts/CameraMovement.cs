e2506a3837fdd0de625efb1666bd303497fca9c7
UsernameCrisis <eugenehandaja@gmail.com>
2025-09-19 19:31:00 +0700

    Added Movement testing and camera movement

---
+++ Zenith/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float yPosOffset;
+    [SerializeField] private GameObject player;
+
+    [Header("Offset")]
+    [SerializeField] private float xOffset = 0f;
+    [SerializeField] private float yOffset = 0f;
+    [SerializeField] private float zOffset = 0f;
+
+    [Header("Rotation")]
+    [SerializeField] private float xRotation = 0f;
+    [SerializeField] private float yRotation = 0f;
+    [SerializeField] private float zRotation = 0f;
+
+    void Awake()
+    {
+        /*
+        IMPORTANT!!!
+
+        The camera Y position and offset is called once because of a bug if it is called every frame.
+        The bug causes the camera to be in a state of falling and snapping back to the offset position.
+        This will cause problems in the future, if height difference for the floor is added.
+        */
+        yPosOffset = player.transform.position.y + yOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x + xOffset, yPosOffset, player.transform.position.z + zOffset);
+    }
+}
---
+++ Zenith/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private InputAction moveAction;
+    private Vector3 moveValue;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float groundDist;
+    [SerializeField] private LayerMask terrainLayer;
+    [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    void Update()
+    {
+        moveValue = moveAction.ReadValue<Vector2>();
+        RayCastToGround();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(moveValue.x * moveSpeed, 0, moveValue.y * moveSpeed));
+    }
+
+    private void RayCastToGround()
+    {
+        RaycastHit hit;
+        Vector3 castPos = transform.position;
+        castPos.y += 1;
+
+        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
+        {
+            if (hit.collider != null)
+            {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + groundDist;
+                transform.position = movePos;
+            }
+        }
+    }
+}

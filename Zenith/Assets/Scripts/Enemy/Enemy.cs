b92ac820e02aff6521b03fb7890872ffe146666f
UsernameCrisis <eugenehandaja@gmail.com>
2025-09-25 12:51:28 +0700

    Added overworld enemy movement and trigger

---
+++ Zenith/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Enemy : Character
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        speed = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

5d0c94cf182b39e4d64be697b4d9d338fb75c20b
Zephylest <kiko.prayogo@gmail.com>
2025-10-16 19:19:08 +0700

    add basic movement animation and idle animations
    add spritesheet character animation

--- Zenith/Assets/Scripts/Player/PlayerMovement.cs
+++ Zenith/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
     private InputAction moveAction;
     private Vector3 moveValue;
+
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float groundDist;
     [SerializeField] private LayerMask terrainLayer;
     [SerializeField] private SpriteRenderer sr;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private Animator animator;
+
     private float defaultMoveSpeed;
+    private float idleTime = 0f;
+
+    private bool canMoveState = true;
 
     void Awake()
     {
@@ -23,12 +29,50 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         moveValue = moveAction.ReadValue<Vector2>();
+
+        bool isRunning = moveValue.magnitude > 0.01f;
+        animator.SetBool("isRunning", isRunning);
+
+        if (!isRunning)
+            idleTime += Time.deltaTime;
+        else
+            idleTime = 0f;
+
+        animator.SetFloat("idleTime", idleTime);
+
+        if (moveValue.x != 0)
+            sr.flipX = moveValue.x < 0;
+
         RayCastToGround();
     }
 
     void FixedUpdate()
     {
-        rb.AddForce(new Vector3(moveValue.x * moveSpeed, 0, moveValue.y * moveSpeed));
+        if (canMoveState)
+            rb.AddForce(new Vector3(moveValue.x * moveSpeed, 0, moveValue.y * moveSpeed));
+    }
+    public void TakeHit()
+    {
+        if (!canMoveState) return;
+        StartCoroutine(HitRoutine());
+    }
+
+    private IEnumerator HitRoutine()
+    {
+        canMove(false);
+        animator.SetBool("isHit", true);
+
+        yield return new WaitForSeconds(0.4f);
+
+        animator.SetBool("isHit", false);
+        canMove(true); 
+    }
+
+    public void canMove(bool canMove)
+    {
+        canMoveState = canMove;
+        moveSpeed = canMove ? defaultMoveSpeed : 0;
+        rb.linearVelocity = Vector3.zero;
     }
 
     private void RayCastToGround()
@@ -45,17 +89,6 @@ public class PlayerMovement : MonoBehaviour
                 movePos.y = hit.point.y + groundDist;
                 transform.position = movePos;
             }
-        }
-    }
-    public void canMove(bool canMove)
-    {
-        if (canMove)
-        {
-            moveSpeed = defaultMoveSpeed;
-        }
-        else
-        {
-            moveSpeed = 0;
         }
     }
 }

849358a05de6d0cb3f033f5e2ee3737f2c3d6cc0
UsernameCrisis <eugenehandaja@gmail.com>
2025-10-20 08:34:14 +0700

    Added enable disable grid visualization
    Enable visualization when the character is clicked, press esc to hide the visualization.
    
    Movement and movement range inside arena is still not yet implemented.

--- Zenith/Assets/Scripts/CombatScripts/GridSelectSystem.cs
+++ Zenith/Assets/Scripts/CombatScripts/GridSelectSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class GridSelectSystem : MonoBehaviour
@@ -5,12 +6,70 @@ public class GridSelectSystem : MonoBehaviour
     [SerializeField] GameObject mouseIndicator, cellIndicator;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private Grid grid;
+    [SerializeField] private ObjectDatabaseSO database;
+    [SerializeField] private GameObject gridVisualization;
+
+    private Vector3 mousePos;
+
+    void OnEnable()
+    {
+        // inputManager.OnHoverEnter += ShowHover;
+        // inputManager.OnHoverExit += HideHover;
+        inputManager.OnColliderClicked += SelectCharacter;
+        inputManager.OnExit += ExitCharacter;
+    }
+
+    void OnDisable()
+    {
+        // inputManager.OnHoverEnter -= ShowHover;
+        // inputManager.OnHoverExit -= HideHover;
+        inputManager.OnColliderClicked -= SelectCharacter;
+        inputManager.OnExit -= ExitCharacter;
+    }
+
+    void Start()
+    {
+        ExitCharacter(); // hanya untuk menghilangkan grid sementara (karena dalam scene view dinyalakan)
+    }
 
     void Update()
     {
-        Vector3 mousePos = inputManager.GetSelectedMapPosition();
-        Vector3Int gridPosition = grid.WorldToCell(mousePos);
+        mousePos = inputManager.GetHoveredMapPosition();
         mouseIndicator.transform.position = mousePos;
+
+        if (gridVisualization.activeSelf)
+            VisualizeHoveredGrid();
+    }
+
+    private void VisualizeHoveredGrid()
+    {
+        Vector3Int gridPosition = grid.WorldToCell(mousePos);
         cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+    private void SelectCharacter(Collider collider)
+    {
+        if (collider.CompareTag("Sprite"))
+        {
+            gridVisualization.SetActive(true);
+            cellIndicator.SetActive(true);
+        }
+        
+    }
+
+    private void ExitCharacter()
+    {
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+    }
+
+    private void HideHover(Collider collider)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ShowHover(Collider collider)
+    {
+        throw new NotImplementedException();
     }
 }
--- Zenith/Assets/Scripts/CombatScripts/InputManager.cs
+++ Zenith/Assets/Scripts/CombatScripts/InputManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
@@ -7,23 +9,93 @@ public class InputManager : MonoBehaviour
     [SerializeField] private LayerMask gridLayerMask;
 
     private Vector3 lastPosition;
-    private InputAction mouseInputPosition;
+    private Collider lastCollider;
+    private InputAction mouseInputPosition, mouseInputLeftClick, escapeKeyAction;
+    public event Action Onclicked, OnExit;
+    public event Action<Collider> OnHoverEnter, OnHoverExit, OnColliderClicked;
 
     void Awake()
     {
         mouseInputPosition = InputSystem.actions.FindAction("MousePosition");
+        mouseInputLeftClick = InputSystem.actions.FindAction("Attack"); // action "Attack" (left click) digunakan karena pakai default InputSystem
+        escapeKeyAction = InputSystem.actions.FindAction("ExitSelect");
     }
 
-    public Vector3 GetSelectedMapPosition()
+    void Update()
     {
-        Vector3 mousePos = new Vector3(mouseInputPosition.ReadValue<Vector2>().x, mouseInputPosition.ReadValue<Vector2>().y, 0);
-        mousePos.z = sceneCamera.nearClipPlane;
-        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100, gridLayerMask))
+        HandleClickInputs();
+        HandleHoverDetection();
+    }
+
+    private void HandleClickInputs()
+    {
+        if (EventSystem.current != null && IsPointerOverUI())
+            return;
+
+        if (mouseInputLeftClick.ReadValue<float>() == 1)
+        {
+            Onclicked?.Invoke();
+
+            Collider clickedCollider = RaycastHoverCollider();
+            if (clickedCollider != null)
+            {
+                OnColliderClicked?.Invoke(clickedCollider);
+            }
+        }
+            
+        if (escapeKeyAction.ReadValue<float>() == 1)
+            OnExit?.Invoke();
+    }
+
+    private void HandleHoverDetection()
+    {
+        if (EventSystem.current != null && IsPointerOverUI())
+            return;
+
+        Collider newCollider = RaycastHoverCollider();
+
+        // Detect hover enter / exit
+        if (newCollider != lastCollider)
+        {
+            if (lastCollider != null)
+                OnHoverExit?.Invoke(lastCollider);
+
+            if (newCollider != null)
+                OnHoverEnter?.Invoke(newCollider);
+
+            lastCollider = newCollider;
+        }
+    }
+    
+    private Collider RaycastHoverCollider()
+    {
+        Vector2 mouseScreenPos = mouseInputPosition.ReadValue<Vector2>();
+        Ray ray = sceneCamera.ScreenPointToRay(mouseScreenPos);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100, gridLayerMask))
         {
             lastPosition = hit.point;
+            return hit.collider;
         }
-        return lastPosition;
+
+        return null;
     }
+
+    public bool IsPointerOverUI() =>
+        EventSystem.current.IsPointerOverGameObject();
+
+    public Vector3 GetHoveredMapPosition() => lastPosition;
+
+    // public Collider GetHoveredCollider()
+    // {
+    //     Vector3 mousePos = new Vector3(mouseInputPosition.ReadValue<Vector2>().x, mouseInputPosition.ReadValue<Vector2>().y, 0);
+    //     mousePos.z = sceneCamera.nearClipPlane;
+    //     Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+    //     RaycastHit hit;
+    //     if (Physics.Raycast(ray, out hit, 100, gridLayerMask))
+    //     {
+    //         lastCollider = hit.collider;
+    //     }
+    //     return lastCollider;
+    // }
 }

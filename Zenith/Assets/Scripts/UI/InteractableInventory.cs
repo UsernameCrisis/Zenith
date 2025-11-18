aa2d4576de2c96eb06183b46fc2e1ab697ebc1f9
R <rianipad2@gmail.com>
2025-11-18 11:52:06 +0700

    chest

--- Zenith/Assets/Scripts/UI/InteractableInventory.cs
+++ Zenith/Assets/Scripts/UI/InteractableInventory.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class InteractableInventory : MonoBehaviour, Interactable
 {
+    public Inventory inventory;
+    public GameObject ChestUI;
     public GameObject Object
     {
         get { return gameObject; }
@@ -10,8 +12,7 @@ public class InteractableInventory : MonoBehaviour, Interactable
 
     public void OnInteract()
     {
-        // transform.parent.GetComponentInParent<SceneRoot>().MainUI.inventory.SetActive(true);
-        // transform.parent.GetComponentInParent<SceneRoot>().ChestUI.gameObject.SetActive(true);
-        // transform.parent.GetComponentInParent<SceneRoot>().MainUI.inventory.GetComponentInChildren<InventoryLeft>().SetInteractableInventoryActive();
+        inventory.SetActive(true);
+        inventory.GetComponentInChildren<InventoryLeft>().SetInteractableInventoryActive();
     }
 }

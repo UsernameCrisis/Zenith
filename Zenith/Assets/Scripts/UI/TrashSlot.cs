b197d7665eb050ac050b6e3edc3fae44e0271526
R <rianipad2@gmail.com>
2025-11-25 20:16:46 +0700

    font

--- Zenith/Assets/Scripts/UI/TrashSlot.cs
+++ Zenith/Assets/Scripts/UI/TrashSlot.cs
@@ -3,8 +3,20 @@ using UnityEngine.EventSystems;
 
 public class TrashSlot : MonoBehaviour, IDropHandler
 {
-    public void OnDrop(PointerEventData eventData)
+    public virtual void OnDrop(PointerEventData eventData)
     {
-        Destroy(eventData.pointerDrag);
+        GameObject dropped = eventData.pointerDrag;
+
+        DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+        draggableItem._parentAfterDrag = transform.GetChild(0);
+
+        if (transform.GetChild(0).childCount > 0)
+        {
+            Destroy(transform.GetChild(0).GetChild(0).gameObject);
+        }
     }
+//     public void OnDrop(PointerEventData eventData)
+//     {
+//         Destroy(eventData.pointerDrag);
+//     }
 }

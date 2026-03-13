80ce693a505c410cf2fa8b79af4bb371720e8793
Zephylest <kiko.prayogo@gmail.com>
2026-03-13 09:45:52 +0700

    add random enemy spawners

---
+++ Zenith/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Enemy Settings")]
+    public GameObject[] enemyPrefabs;
+
+    [Range(0f, 1f)]
+    public float spawnProbability = 0.5f;
+
+    void Start()
+    {
+        TrySpawn();
+    }
+
+    public void TrySpawn()
+    {
+        float roll = Random.value;
+
+        if (roll <= spawnProbability)
+        {
+            int randomIndex = Random.Range(0, enemyPrefabs.Length);
+            Instantiate(enemyPrefabs[randomIndex], transform.position, Quaternion.identity);
+            Debug.Log("Spawned: " + enemyPrefabs[randomIndex].name);
+        }
+    }
+}
